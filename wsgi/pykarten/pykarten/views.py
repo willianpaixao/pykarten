f3dd82fcfe6ddb4b18cc49bee973f5e7c240e240
Willian Paixao <willian@ufpa.br>
2015-05-27 00:29:28 -0300

    pykarten Django project added.
    myproject was replaced.

---
+++ wsgi/pykarten/pykarten/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
--- wsgi/pykarten/pykarten/wsgi.py
+++ wsgi/pykarten/pykarten/wsgi.py
@@ -1,15 +1,16 @@
 """
-WSGI config for myproject project.
+WSGI config for pykarten project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-# GETTING-STARTED: change 'myproject' to your project name:
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pykarten.settings")
+
 application = get_wsgi_application()
