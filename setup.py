e6aeccc73075f4bc13084d97bc6cfd115f8aaad9
Willian Paixao <willian@ufpa.br>
2015-05-27 00:37:07 -0300

    Last settings.
    setup.py and application files needed to be filled and refactored with the right
    settings.

--- setup.py
+++ setup.py
@@ -3,18 +3,13 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
-    name='YourAppName',
-    # GETTING-STARTED: set your app version:
-    version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='OpenShift App',
-    # GETTING-STARTED: set author name (your name):
-    author='Your Name',
-    # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
-    # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
-    install_requires=['Django<=1.8'],
+    name='pykarten',
+    version='0.01',
+    description='Flashcard generator web app',
+    author='Willian Paixao',
+    author_email='willian@ufpa.br',
+    url='http://python-pykarten.rhcloud.com/',
+    install_requires=['django==1.8', 'django-filter', 'django-zurb-foundation',
+        'fabric', 'psycopg2'],
+    license='GPLv3',
 )
